bff05af57427c392632bcad410731c40034777d6
jheftmann <jheftmann@mac.com>
2015-01-25 00:18:02 -0500

    remove partials

--- index.php
+++ index.php
@@ -1,10 +1,93 @@
-<?php include ('snippets/_header.php'); ?>
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Core Interaction: Spring 2015</title>
+	<!-- favicon -->
+	<link rel="icon" sizes="16x16 32x32" href="/path/to/favicon.ico?v=2">
+	<!--[if IE]><link rel="shortcut icon" href="/path/to/favicon.ico?v=2"><![endif]-->
+	<link rel="apple-touch-icon-precomposed" href="path/to/favicon-152.png?v=2">
+	<!-- description -->
+	<meta name="description" content="one, two, three" />
+	<!-- keywords -->
+	<meta name="keywords" content="one, two, three" />
+	<!-- viewport -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui">
+	<!-- rewrite URLs for @2x retina images -->
+	<script>
+	    if((window.devicePixelRatio===undefined?1:window.devicePixelRatio)>1)
+		document.cookie='HTTP_IS_RETINA=1;path=/';
+	</script>
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- css -->
+	<link href="styles/site.css" rel="stylesheet">
+
+	</head>
+	<body>
+
 
 
 	<div class="container-fluid">
 		<div class="inner">
 
-			<?php include ('snippets/sidebar.php'); ?>
+			<div class="sidebar">
+				<header>
+					<h5><a href="/">
+						Core Interaction
+						<br>
+						Spring 2015
+						</a>
+					</h5>
+					<h5>
+						Studio
+						<br>
+						PUCD 2126
+						<br>
+						Tues./Thurs. 1900&#8212;2140
+						<br>
+						Room U 306
+						<br>
+						Minnie Choi
+						<br>
+						<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
+					</h5>
+					<h5>
+						Lab
+						<br>
+						PUCD 2126
+						<br>
+						Mon. 1900&#8212;2140
+						<br>
+						Room U 306
+						<br>
+						Jacob Heftmann
+						<br>
+						<a href="mailto:heftmannj@newschool.edu">email@newschool.edu</a>
+					</h5>
+					<h5>
+
+					</h5>
+				</header>
+				<nav>
+					<ul>
+						<li><span class="indicator"></span><a href="#description">Description</a></li>
+						<li><span class="indicator"></span><a href="#calendar">Calendar</a></li>
+						<li><span class="indicator"></span><a href="#resources">Resources</a></li>
+						<li><span class="indicator"></span><a href="#grading">Grading</a></li>
+						<li><span class="indicator"></span><a href="#policies">Policies</a></li>
+					</ul>
+					<ul class="subnav">
+						<li><a href="#">Syllabus (PDF)</a></li>
+						<li><a href="#">Code snippets</a></li>
+					</ul>
+				</nav>
+			</div>
 
 			<div class="content">
 				<header class="site-header">
@@ -461,4 +544,13 @@
 
 
 
-<?php include ('snippets/_footer.php'); ?>
+		<div class="footer">
+			<h6><a id="top">Top</a></h5>
+		</div>
+
+		<!-- js -->
+		<script src="scripts/lib.min.js"></script>
+		<script src="scripts/site.js"></script>
+
+	</body>
+</html>
